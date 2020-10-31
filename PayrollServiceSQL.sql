58795147ec3d4d8587565226c44c857406f52667
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-31 07:37:28 +0530

    [Tirupathi] Add. DataTable created UC2

--- PayrollServiceSQL.sql
+++ PayrollServiceSQL.sql
@@ -3,3 +3,11 @@
 /*UC1-Creating Database*/
 create database payroll_service;
 use payroll_service;
+
+/*UC2-Creating DataTable*/
+create table employee_payroll(
+id int NOT NULL identity(1,1) PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+start_Date DATETIME NOT NULL ) ;
+
